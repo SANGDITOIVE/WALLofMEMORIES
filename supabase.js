11145eb3e83b1b9e0097313649ee5469d52f9e03
SANGDITOIVE <lequangdai.spc@gmail.com>
2026-03-11 21:30:42 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://ifarghceexpsfyfatjkb.supabase.co"
 
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+const SUPABASE_KEY = "sb_publishable_hurMK-loaGZ-aUMzFgnHow_0wOqspLL"
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
