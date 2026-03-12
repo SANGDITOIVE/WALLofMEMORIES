8cdaf8a941721d2d50b825b50fdeead31153dedc
SANGDITOIVE <lequangdai.spc@gmail.com>
2026-03-12 21:51:49 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,18 @@
 const SUPABASE_URL = "https://ifarghceexpsfyfatjkb.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmYXJnaGNlZXhwc2Z5ZmF0amtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNTkyNDQsImV4cCI6MjA4ODczNTI0NH0.QRxgy_NDmHGGNOP3Di-zW5LlDy_sKVdSjbGa2zjk2qQ"
 
-const SUPABASE_KEY = "sb_publishable_hurMK-loaGZ-aUMzFgnHow_0wOqspLL"
+if (!window.supabase) {
+  console.error('Supabase library not loaded!');
+}
 
-const supabaseClient = window.supabase.createClient(
+const supabaseClient = window.supabase?.createClient(
   SUPABASE_URL,
-  SUPABASE_KEY
+  SUPABASE_KEY,
+  {
+    auth: {
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: true
+    }
+  }
 )
